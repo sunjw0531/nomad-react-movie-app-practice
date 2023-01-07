1407f0498451fc294ef83420ff16ce7158a481e6
정승우 <sunjw0531@gmail.com>
2023-01-07 22:45:27 +0900

    useEffect 1
    If we use useEffect, its first argument will be run only once when the codes rendered.

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
+  // run everytime when rerendered
   console.log('rendered');
+  // run only once when rendered
+  useEffect(() => {
+    console.log('Call the API!');
+  }, []);
+
   return (
     <div>
       <h1>{counter}</h1>
