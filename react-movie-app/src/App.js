4ce324e1d6f4137b63069cb9fba212a458e80bd8
정승우 <sunjw0531@gmail.com>
2023-01-07 23:23:48 +0900

    useEffect 2
    The second argument in useEffect means the state that makes run the useEffect when it changed.

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -2,18 +2,34 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
-  // run everytime when rerendered
-  console.log('rendered');
-  // run only once when rendered
+
   useEffect(() => {
-    console.log('Call the API!');
+    console.log('I run only once!');
   }, []);
 
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when counter & keyword changes.');
+  }, [counter, keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value);
+        }}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
