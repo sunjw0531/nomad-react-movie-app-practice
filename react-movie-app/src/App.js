d7d13e19940a51e7f7b37484f6c0af36a81dc4cb
정승우 <sunjw0531@gmail.com>
2023-01-08 11:34:48 +0900

    cleanup
    If we use 'return' in useEffect, then the component in 'return' will be run when the useEffect is destroied.

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,37 +1,27 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => {
-    setValue((prev) => prev + 1);
-  };
+  const [showing, setShowing] = useState(false);
 
-  useEffect(() => {
-    console.log('I run only once!');
-  }, []);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log('I run when counter & keyword changes.');
-  }, [counter, keyword]);
+  function Hello() {
+    useEffect(() => {
+      console.log('hi');
+      return () => {
+        console.log('bye');
+      };
+    }, []);
+    return <h1>Hello</h1>;
+  }
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={(e) => {
-          setKeyword(e.target.value);
+      {showing ? <Hello /> : null}
+      <button
+        onClick={() => {
+          setShowing((prev) => !prev);
         }}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      >
+        {showing ? 'Hide' : 'Show'}
+      </button>
     </div>
   );
 }
