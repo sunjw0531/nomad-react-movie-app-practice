ad100d44bb244bb97a78d9851a719bc3c5a94f43
정승우 <sunjw0531@gmail.com>
2023-01-11 20:33:27 +0900

    simple movie app 1
    using async await for getting data from API

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -2,60 +2,40 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState(0);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The Coins! There are {coins.length} coins.</h1>
-      <h2>
-        If you want to exchange USD to Coins, then write your money and choose
-        the coin
-      </h2>
-      <input
-        placeholder="write money here"
-        onChange={(e) => {
-          setMoney(Number(e.target.value));
-        }}
-      />
-
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>loading...</h1>
       ) : (
-        <>
-          {/* <select onChange={(e) => console.log(e.target.value)}> */}
-          <select
-            onChange={(e) => {
-              const data = JSON.parse(e.target.value);
-              setSelectedCoin([data.name, data.quotes.USD.price]);
-            }}
-          >
-            {coins.map((coin, index) => {
-              // return (
-              //   <option key={index} value={coin.quotes.USD.price}>
-              //     {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              //   </option>
-              // );
-              return (
-                <option key={index} value={JSON.stringify(coin)}>
-                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-                </option>
-              );
-            })}
-          </select>
-          <h2>
-            Your money : ${money} and you choose {selectedCoin[0]}. <br />
-            You can buy {selectedCoin[0]} : {money / selectedCoin[1]}
-          </h2>
-        </>
+        <div>
+          {movies.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <img src={movie.medium_cover_image} />
+                <h1>{movie.title}</h1>
+                <p>{movie.summary}</p>
+                <ul>
+                  {movie.genres.map((g) => (
+                    <li key={g}>{g}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
       )}
     </div>
   );
